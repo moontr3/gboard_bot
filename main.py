0af2d823329dd2825155886361b488e53c98bd31
moontr3 <99617919+moontr3@users.noreply.github.com>
2022-05-27 19:37:59 +0300

    Issue fix
    Removed unnecessary libraries

--- main.py
+++ main.py
@@ -2,8 +2,6 @@
 
 #  MODULES / LIBRARIES  ##############################################################################################
 
-from asyncore import read
-from email.iterators import body_line_iterator
 import discord
 from discord.ext import commands
 import re
